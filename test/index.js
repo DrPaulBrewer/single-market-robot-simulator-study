59250fba6e3db8092943c837cf1461a6bf6e1091
Dr Paul Brewer <drpaulbrewer@gmail.com>
2018-03-01 03:38:44 -0500

    test .isMorphable with array values

--- test/index.js
+++ test/index.js
@@ -283,7 +283,14 @@ describe('Study ', function(){
 		[ {a:1,b:2}, {b:2}, true],
 		[ {a:1,b:3,c:1}, {b:1}, true],
 		[ {a:1,b:"yellow",c:1}, {b:1}, false],
-		[ {a:1,b:"yellow",c:1}, {b:"orange"}, false]
+		[ {a:1,b:"yellow",c:1}, {b:"orange"}, false],
+		[ {a:[1,2,3]}, {a: null}, false],
+		[ {a:[1,2,3]}, {a:{foo: "bar"}}, false],
+		[ {a:[1,2,3]}, {a: [3,4,5]}, true],
+		[ {a:['x','y','z']}, {a: [3,4,5]}, false],
+		[ {a:['x','y','z']}, {a: ['q','q','q']}, true],
+		[ {a:['x','y','z']}, {a: [null,'x',3]}, false],
+		[ {a:[null,null,null]}, {a: [null,null,null]}, false]
 	];
 	tests.forEach(([A,B,expected])=>{
 		it(` A = ${A} B=${B} --> ${expected} `, function(){
